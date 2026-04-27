df8558440a73587ef43a48d2e7d9d450fe70948d
wangwenzhe <wangwenzhe163@163.com>
2026-04-27 09:47:46 +0800

    hhxdn-dev##放行okx

--- ruoyi-framework/src/main/java/com/ruoyi/framework/config/SecurityConfig.java
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/config/SecurityConfig.java
@@ -24,7 +24,7 @@ import com.ruoyi.framework.security.handle.LogoutSuccessHandlerImpl;
  * 
  * @author ruoyi
  */
-@EnableMethodSecurity(prePostEnabled = true, securedEnabled = true)
+@EnableMethodSecurity(prePostEnabled = true, securedEnabled = true,jsr250Enabled = true)
 @Configuration
 public class SecurityConfig
 {
@@ -100,7 +100,7 @@ public class SecurityConfig
             .authorizeHttpRequests((requests) -> {
                 permitAllUrl.getUrls().forEach(url -> requests.requestMatchers(url).permitAll());
                 // 对于登录login 注册register 验证码captchaImage 允许匿名访问
-                requests.requestMatchers("/login", "/register", "/captchaImage").permitAll()
+                requests.requestMatchers("/login", "/register", "/captchaImage","/okx/**").permitAll()
                     // 静态资源，可匿名访问
                     .requestMatchers(HttpMethod.GET, "/", "/*.html", "/**.html", "/**.css", "/**.js", "/profile/**").permitAll()
                     .requestMatchers("/swagger-ui.html", "/v3/api-docs/**", "/swagger-ui/**", "/druid/**").permitAll()
